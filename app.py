5e2178972c51b3070c7e48771da7f1b6591b725b
Sudip Debnath <developer.sudip07@gmail.com>
2026-05-09 04:23:57 +0530

    app updated, fixed download function from mobile browsers

--- app.py
+++ app.py
@@ -371,8 +371,19 @@ if "formats" in st.session_state:
 
                 "extractor_args": {
                     "youtube": {
-                        "player_client": ["android", "web"]
+                        "player_client": ["ios", "android", "mweb"],
+                        "player_skip_bundle_url": True,
+                    },
+                    "instagram": {
+                        "check_display_resources": True,
                     }
+                },
+
+                "http_headers": {
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/121.0.0.0 Safari/537.36",
+                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+                    "Accept-Language": "en-US,en;q=0.9",
+                    "Referer": "https://www.google.com/",
                 },
 
                 "http_headers": {
